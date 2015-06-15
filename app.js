8c4208e341d7402fdf50cb2fab3b3c37377e41f2
JoseLuisTo <jltomico@gmail.com>
2015-06-15 20:40:33 +0200

    Primera pregunta

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+//var routes = require('./routes/index');
 var routes = require('./routes/index');
 
 var app = express();
@@ -22,6 +23,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//app.use('/', routes);
 app.use('/', routes);
 
 // catch 404 and forward to error handler
