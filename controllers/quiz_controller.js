520197adb6707b4f7c2d8ff5e5dc41c7d30ad19f
JoseLuisTo <jltomico@gmail.com>
2015-06-23 19:52:56 +0200

    Buscador de Preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -13,12 +13,22 @@ exports.load = function(req, res, next, quizId) {
 };
 
 // GET /quizes
-exports.index = function(req, res) {
-  models.Quiz.findAll().then(
-    function(quizes) {
-      res.render('quizes/index', { quizes: quizes});
-    }
-  ).catch(function(error) { next(error);})
+exports.index = function(req, res){
+	console.log("*******"+req.query.search)
+	if(req.query.search === undefined){
+		models.Quiz.findAll({order: ["pregunta"]}).then(function(quizes) {
+		res.render('quizes/index.ejs', { quizes: quizes});
+	}).catch(function(error){ next(error);});
+	} else {
+		models.Quiz.findAll(
+				{
+				 where: ["lower(pregunta) like ?", "%"+req.query.search.replace(" ","%")+"%"],
+				 order: ["pregunta"]
+				}
+		).then(function(quizes) {
+		res.render('quizes/index.ejs', { quizes: quizes});
+	}).catch(function(error){ next(error);});
+	}
 };
 
 // GET /quizes/:id
