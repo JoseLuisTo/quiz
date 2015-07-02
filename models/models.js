bd42d5cd74fe4c9b404fc183c84565a71ddcc158
JoseLuisTo <jltomico@gmail.com>
2015-07-02 19:14:54 +0200

    Crear comentario

--- models/models.js
+++ models/models.js
@@ -29,7 +29,14 @@ var sequelize = new Sequelize(DB_name, user, pwd,
 // Importar la definicion de la tabla Quiz en quiz.js
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 
+// Importar definicion de la tabla Comment
+var Comment = sequelize.import(path.join(__dirname,'comment'));
+
+Comment.belongsTo(Quiz); //establece relacion entre tablas
+Quiz.hasMany(Comment); //establece relacion entre tablas
+
 exports.Quiz = Quiz; // exportar definición de tabla Quiz
+exports.Comment = Comment;
 
 // sequelize.sync() crea e inicializa tabla de preguntas en DB
 sequelize.sync().then(function() {
